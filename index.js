a76931b2aefa371ed9035ed92725af14e6de3ed3
Claudia Stevens <71283725+claudiajstevens@users.noreply.github.com>
2022-08-24 04:12:10 +0000

    project satisfies all user stories

--- index.js
+++ index.js
@@ -28,3 +28,15 @@ app.get('/api/hello', function (req, res) {
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+app.get('/api/whoami', function(req, res){
+  const ip = req.socket.localAddress;
+  const lang = req.headers["accept-language"]; 
+  const sw = req.headers["user-agent"];
+
+  res.json({
+    ipaddress: ip,
+    language: lang,
+    software: sw
+  });
+});
